af9b3faeb6f9adfdc1a039d42d081f4bf2a52a1d
Horace Williams <horace.d.williams@gmail.com>
2018-09-08 13:24:29 -0700

    Mapbox init and some other deps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl';
+
+const mapboxToken = 'pk.eyJ1Ijoid29yYWNlIiwiYSI6ImNqbHR1OXpvMjA0MWwza2xpeGp6ODk5NG8ifQ.U_Hnl80dqIsQJknQtC6CVQ';
+mapboxgl.accessToken = mapboxToken;
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <div ref={mc => this.mapContainer = mc} className="mapbox-container" />
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
   }
 }
 
